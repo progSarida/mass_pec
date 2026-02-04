a4abad3031815e726d38dd3cb92a09118574c3ef
progSarida <programmazione@sarida.it>
2026-02-04 12:42:45 +0100

    Correzione

--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,13 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 
 // ROTTE PER LA LOGIN CENTRALIZZATA
-// Route::get('/auth/callback', [SsoController::class, 'callback'])->name('sso.callback');
-// Route::get('/sso-login', [SsoController::class, 'redirect'])->name('sso.login');
-// Route::post('/slo-callback', [SsoController::class, 'handleSloCallback'])->name('sso.logout')->withoutMiddleware([VerifyCsrfToken::class]);
+Route::get('/auth/callback', [SsoController::class, 'callback'])->name('sso.callback');
+Route::get('/sso-login', [SsoController::class, 'redirect'])->name('sso.login');
+Route::post('/slo-callback', [SsoController::class, 'handleSloCallback'])->name('sso.logout')->withoutMiddleware([VerifyCsrfToken::class]);
 
-// Route::get('/admin/login', fn() => redirect()->route('sso.login'))->name('filament.admin.auth.login');
+Route::get('/admin/login', fn() => redirect()->route('sso.login'))->name('filament.admin.auth.login');
 
-// Route::get('/login', fn() => redirect()->route('sso.login'));
+Route::get('/login', fn() => redirect()->route('sso.login'));
 
 // ROTTE DI UTILITA'
 // scarico zip degli allegati
