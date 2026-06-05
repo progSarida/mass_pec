7ea3a115db1473f9f1b37f3896bdb0fae6b7deec
progSarida <programmazione@sarida.it>
2026-06-05 09:51:27 +0200

    Inserimento temporaneo route per recupero ip pubblico

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,15 @@ Route::get('/download-zip/{type}/{id}', [AttachmentController::class, 'downloadZ
 Route::get('/download-zip-receipts/{id}', [AttachmentController::class, 'downloadZipReceipts'])->name('receipts.zip')->middleware(['auth']);
 // scarico zip dei documenti integrativi
 Route::get('/download-zip-related/{id}', [AttachmentController::class, 'downloadZipRelated'])->name('related.zip')->middleware(['auth']);
+
+
+// ROTTA DI TEST PER VERIFICA IP PUBBLICO E GEOLOCALIZZAZIONE
+Route::get('/my-ip', function () {
+    $ip = file_get_contents('https://ifconfig.me/ip');
+    $info = file_get_contents('https://ipinfo.io/' . $ip);
+    
+    return response()->json([
+        'ip' => trim($ip),
+        'info' => json_decode($info, true)
+    ]);
+});
