d538ca3e91a4c270c904a5fe3b4dc24a8891ad11
progSarida <programmazione@sarida.it>
2026-06-19 16:50:54 +0200

    Tempporaneo per test ip

--- routes/web.php
+++ routes/web.php
@@ -23,40 +23,40 @@ Route::get('/download-zip-receipts/{id}', [AttachmentController::class, 'downloa
 Route::get('/download-zip-related/{id}', [AttachmentController::class, 'downloadZipRelated'])->name('related.zip')->middleware(['auth']);
 
 // ROTTA DI TEST PER VERIFICA IP PUBBLICO V6 E GEOLOCALIZZAZIONE
-// Route::get('/my-ip', function () {
-//     $ip = file_get_contents('https://ifconfig.me/ip');
-//     $info = file_get_contents('https://ipinfo.io/' . $ip);
+Route::get('/my-ip', function () {
+    $ip = file_get_contents('https://ifconfig.me/ip');
+    $info = file_get_contents('https://ipinfo.io/' . $ip);
     
-//     return response()->json([
-//         'ip' => trim($ip),
-//         'info' => json_decode($info, true)
-//     ]);
-// });
+    return response()->json([
+        'ip' => trim($ip),
+        'info' => json_decode($info, true)
+    ]);
+});
 
 // ROTTA DI TEST PER VERIFICA IP PUBBLICO V4 E GEOLOCALIZZAZIONE
-// use Illuminate\Support\Facades\Http;
-// Route::get('/my-ip', function () {
-//     // Utilizziamo il client HTTP di Laravel forzando l'uso di IPv4 (CURLOPT_IPRESOLVE)
-//     $response = Http::withOptions([
-//         'curl' => [
-//             CURLOPT_IPRESOLVE => CURL_IPRESOLVE_V4
-//         ]
-//     ])->get('https://ipinfo.io/json');
+use Illuminate\Support\Facades\Http;
+Route::get('/my-ip', function () {
+    // Utilizziamo il client HTTP di Laravel forzando l'uso di IPv4 (CURLOPT_IPRESOLVE)
+    $response = Http::withOptions([
+        'curl' => [
+            CURLOPT_IPRESOLVE => CURL_IPRESOLVE_V4
+        ]
+    ])->get('https://ipinfo.io/json');
 
-//     if ($response->failed()) {
-//         return response()->json(['error' => 'Impossibile recuperare i dati'], 500);
-//     }
+    if ($response->failed()) {
+        return response()->json(['error' => 'Impossibile recuperare i dati'], 500);
+    }
 
-//     // Recupero i dati con IPv6
-//     $ip = file_get_contents('https://ifconfig.me/ip');
-//     $info = file_get_contents('https://ipinfo.io/' . $ip);
+    // Recupero i dati con IPv6
+    $ip = file_get_contents('https://ifconfig.me/ip');
+    $info = file_get_contents('https://ipinfo.io/' . $ip);
 
-//     $data = $response->json();
+    $data = $response->json();
 
-//     return response()->json([
-//         'ip_v4'   => $data['ip'] ?? null,
-//         'info_v4' => $data,
-//         'ip_v6' => trim($ip),
-//         'info_v6' => json_decode($info, true)
-//     ]);
-// });
+    return response()->json([
+        'ip_v4'   => $data['ip'] ?? null,
+        'info_v4' => $data,
+        'ip_v6' => trim($ip),
+        'info_v6' => json_decode($info, true)
+    ]);
+});
